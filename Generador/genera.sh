796dae0b703412505a5547dd4b63d54155bc1646
octavi.pascual <octavi.pascual@est.fib.upc.edu>
2016-01-03 20:46:40 +0100

    Script

---
+++ Generador/genera.sh
@@ -0,0 +1,12 @@
+#!/bin/bash       
+for pow in {2..6}
+do
+	for V in 2 3 4
+	do
+		for P in 50 70 90 100
+		do
+			N=$((10**$pow))
+			./generador.exe $N $V $P "dict-$N-$V-$P" "text-$N-$V-$P" 1234
+		done
+	done
+done
